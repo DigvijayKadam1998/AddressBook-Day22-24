722db2ead2534b21eaa88b2ede39b63949b9c351
[Digvikadam] <[digvikadam168@gmail.com]>
2022-01-18 17:36:44 +0530

    Added: Add details through Console

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -5,19 +5,11 @@ public class AddressBookMain {
 		System.out.println("Welcome to AddressBook Program");
 
 		AddressBook contact = new AddressBook();
-		contact.setFirstName("Digvijay");
-		contact.setLastName("Kadam");
-		contact.setAddress("Satara pin-415518");
-		contact.setCity("Satara");
-		contact.setState("Maharashtra");
-		contact.setZip("415518");
-		contact.setPhoneNumber("8669057912");
-		contact.setEmail("digvijaykadam007@gmail.com");
+		UserInterface userInterface = new UserInterface();
+		userInterface.enterDetails(contact);
 
 		ContactStore contactStore = new ContactStore();
 		contactStore.add(contact);
-
-		UserInterface userInterface = new UserInterface();
 		userInterface.print(contactStore.getContactList());
 	}
 }
